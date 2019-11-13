720b10aeac9dadbd61062a3fdecdbc1b35bc8214
burakakgun0 <burakakgun0@gmail.com>
2019-11-13 21:48:45 +0300

    bildirimVe mesaj sayi

--- header2.php
+++ header2.php
@@ -19,7 +19,7 @@
               <li class="dropdown"><a href="newsfeed">Anasayfa</a></li>
                
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div  class="pull-right"><span class="label label-danger">1</span></div> Bildirimler &nbsp;<span></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div  class="pull-right"><span  id="bildirimler" class="label label-danger">1</span></div> Bildirimler &nbsp;<span></span></a>
                 <ul class="dropdown-menu login">
                  <li><a><p>asdkasjdlk</p></a>
 <a><p>asdkasjdlk</p></a><a><p>asdkasjdlk</p></a><a><p>asdkasjdlkasdasdasdasdasdasd</p></a><a><p>asdkasjdlk</p></a><a><p>asdkasjdlk</p></a>
@@ -32,7 +32,7 @@
 
 
               </li>
-              <li class="dropdown"><a href="messages">Mesajlar &nbsp;<div  class="pull-right"><span class="label label-danger">1</span></div></a></li>
+              <li class="dropdown"><a href="messages">Mesajlar &nbsp;<div  class="pull-right"><span  id="mesaj" class="label label-danger">1</span></div></a></li>
               <li class="dropdown"><a href="group">Gruplar</a></li>
 
               <li class="dropdown"><a href="<?=$sesCek['username'] ?>"><img style="width: 30px; height: 30px; float: left; margin-right: 5px;" src="../images/users/user-2.jpg" alt="user" class="img-responsive profile-photo">  <?=$sesCek['name'] ?></a></li>
@@ -49,3 +49,72 @@
         </div><!-- /.container -->
       </nav>
     </header>
+	
+		<script>
+	  $(document).ready(function(){
+                                                mesajGetir = function ()
+                                                {
+													var msg="";
+													var bild="";
+													var degerler="";
+                                                                $.ajax({
+                                                                type: "POST", 
+                                                                url: "config/mesajVeBildirimSayıCek.php", 
+                                                                data : degerler,																
+                                                                success:function(data){
+																
+																	abc = JSON.parse(data);
+																				console.log(abc);																
+																	msg=abc[0].mesaj;
+																		bild=abc[0].bildirim;
+																		$("#bildirimler").html(bild);
+																		$("#mesaj").html(msg);
+																
+															
+                                                            }
+
+                                                        });
+	
+                                                }
+												//mesaj at
+												  
+                                             });
+	
+	
+	
+
+
+
+	
+
+	
+	function bildirimVeMesajSayıÇek(id)
+	{
+		
+		var a =document.getElementById("to"+id).value;
+		var s =mesajCek(id,a);
+		var s =mesajGetir(id);
+			updateScroll();
+
+	}
+	
+	
+	window.onload = function(){ 
+   
+	mesajGetir();
+
+}
+
+	
+	
+	setInterval(function(){ baslikCek() }, 30000);
+	
+	</script>
+
+    <!-- Scripts
+    ================================================= -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky-kit.min.js"></script>
+    <script src="js/jquery.scrollbar.min.js"></script>
+    <script src="js/script.js"></script>
